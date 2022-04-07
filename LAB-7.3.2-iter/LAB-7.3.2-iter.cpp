ad1b52c206228654c940c463ad87eb7672f2ff99
Sushomeister <91095463+Sushomeister@users.noreply.github.com>
2022-04-07 10:29:30 +0300

    Update LAB-7.3.2-iter.cpp

--- LAB-7.3.2-iter/LAB-7.3.2-iter.cpp
+++ LAB-7.3.2-iter/LAB-7.3.2-iter.cpp
@@ -3,7 +3,7 @@
 // Лабораторна робота №7.3
 // Опрацювання динамічних багатовимірних масивів.
 // Варіант 18
-// Ітераційний спосіб.
+// Ітераційний спосіб
 
 #include <iostream>
 #include <iomanip>
@@ -21,8 +21,8 @@ int main()
 
 	srand((unsigned)time(NULL));
 
-	int Low = -12;
-	int High = 23;
+	int Low = 0;
+	int High = 5;
 
 	int k, n;
 
@@ -35,13 +35,12 @@ int main()
 
 	Random(a, k, n, Low, High);
 	Print(a, k, n);
-	int b = 0;
-	int max = 0;
+	int b = 1;
+	int max = 1;
 	int c = 0;
 
 	Part2_Number(a, k, n, b, max, c);
 	cout << "C = " << c << endl;
-
 
 	for (int i = 0; i < k; i++)
 		delete[] a[i];
@@ -72,23 +71,25 @@ void Print(int** a, const int k, const int n)
 void Part2_Number(int** a, const int k, const int n, int b, int max, int& c)
 {
 	c = 0;
-	b = 0;
+	b = 1;
 
-	for (int i = 0; i < k; i++)
+	for (int j = 0; j < n; j++)
 	{
-		for (int j = 0; j < n - 1; j++)
+		b = 1;
+		for (int i = 0; i < k - 1; i++)
 		{
-			if (a[i][j] == a[i][j + 1])
+			if (a[i][j] == a[i + 1][j])
 			{
 				b++;
 
 				if (b >= max)
 				{
 					max = b;
-					c = i++;
-					break;
+					c = j;
 				}
 			}
+			else
+				b = 1;
 		}
 	}
 }
